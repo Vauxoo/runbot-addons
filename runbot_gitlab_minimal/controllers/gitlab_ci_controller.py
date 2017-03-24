73c203cfd808df2bb5129f4b5119ad258a79fa05
JesusZapata <jesus@vauxoo.com>
2017-03-24 18:10:23 +0000

    [REF] runbot_gitlab_minimal: Delete repeated code and call the parent method

--- runbot_gitlab_minimal/controllers/gitlab_ci_controller.py
+++ runbot_gitlab_minimal/controllers/gitlab_ci_controller.py
@@ -2,23 +2,21 @@
 # Copyright <2017> <Vauxoo info@vauxoo.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import datetime
-import pprint
 import logging
 
-import openerp
 from openerp import http, SUPERUSER_ID
 from openerp.http import request
+from openerp.addons.runbot import runbot
 
 _logger = logging.getLogger(__name__)
 
 
-class RunbotCIController(http.Controller):
+class RunbotCIController(runbot.RunbotController):
 
     @http.route(['/runbot/hook_gitlab/<int:repo_id>',
                  '/runbot/hook_gitlab/org'], type='json', auth="public",
                 website=True)
-    def hook(self, repo_id=None, **post):
+    def hook_gitlab(self, repo_id=None, **post):
         data = request.jsonrequest
         event = data['object_kind'] if data.has_key('object_kind') else None
         repository = data['repository']
@@ -33,13 +31,5 @@ class RunbotCIController(http.Controller):
                                                               repo_domain,
                                                               limit=1)
                 repo_id = repo[0] if len(repo) else None
-        if repo_id:
-            repo = request.registry['runbot.repo'].browse(request.cr,
-                                                          SUPERUSER_ID,
-                                                          [repo_id])
-            repo.hook_time = datetime.datetime.now().strftime(
-                openerp.tools.DEFAULT_SERVER_DATETIME_FORMAT)
-        else:
-            _logger.debug('Repo not found from request data: %s',
-                          pprint.pformat(request.jsonrequest)[:450])
+        super(RunbotCIController, self).hook(repo_id=None, **post)
         return {}
