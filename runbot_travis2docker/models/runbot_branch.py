c384ddfc900624304e57313a53e87148daa0c2c8
Jesus Zapata <jesus@vauxoo.com>
2017-04-24 19:38:12 +0000

    [IMP] runbot_travis2docker: Calculate the date time of the last modification on weblate for made the build

--- runbot_travis2docker/models/runbot_branch.py
+++ runbot_travis2docker/models/runbot_branch.py
@@ -3,6 +3,11 @@
 #   Coded by: moylop260@vauxoo.com
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from datetime import datetime
+import requests
+
+from urlparse import urlparse
+
 from openerp import fields, models, api
 
 
@@ -10,13 +15,55 @@ class RunbotBranch(models.Model):
     _inherit = "runbot.branch"
 
     uses_weblate = fields.Boolean(help='Synchronize with Weblate')
+    updated_weblate = fields.Datetime(help='Last update of weblate')
 
     @api.model
     def cron_weblate(self):
         for branch in self.search([('uses_weblate', '=', True)]):
-            self.env['runbot.build'].create({'branch_id': branch.id,
-                                             'name': 'HEAD',
-                                             'uses_weblate': True})
+            if (not branch.repo_id.weblate_token or
+                    not branch.repo_id.weblate_url):
+                continue
+            current_date = (datetime.strptime(branch.updated_weblate,
+                                              '%Y-%m-%d %H:%M:%S')
+                            if branch.updated_weblate
+                            else None)
+            new_date = None
+            url = branch.repo_id.weblate_url
+            session = requests.Session()
+            session.headers.update({
+                'Accept': 'application/json',
+                'User-Agent': 'runbot_travis2docker',
+                'Authorization': 'Token %s' % branch.repo_id.weblate_token
+            })
+            projects = session.get(url + '/projects/').json()
+            for project in projects['results']:
+                url_info = urlparse(project['web'])
+                if not url_info.path[1:] in branch.repo_id.name:
+                    continue
+                components = session.get('%s/projects/%s/components'
+                                         % (url, project['slug'])).json()
+                for component in components['results']:
+                    if component['branch'] != branch.branch_name:
+                        continue
+                    changes = session.get('%s/components/%s/%s/changes/'
+                                          % (url, project['slug'],
+                                             component['slug'])).json()
+                    if not changes['results']:
+                        continue
+                    change = changes['results'].pop()
+                    date = datetime.strptime(
+                        change['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
+                    new_date = (date
+                                if (not new_date or date > new_date)
+                                else new_date)
+            if ((current_date and new_date and
+                 current_date < new_date.replace(microsecond=0)) or
+                    (not current_date and new_date)):
+                branch.write({'updated_weblate':
+                              new_date.strftime('%Y-%m-%d %H:%M:%S')})
+                self.env['runbot.build'].create({'branch_id': branch.id,
+                                                 'name': 'HEAD',
+                                                 'uses_weblate': True})
 
     def _get_branch_quickconnect_url(self, cr, uid, ids, fqdn, dest,
                                      context=None):
