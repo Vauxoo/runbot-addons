40afb9640e308f814e4578f85d6680778181f00e
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2017-05-07 20:50:10 -0500

    Update runbot_branch.py

--- runbot_travis2docker/models/runbot_branch.py
+++ runbot_travis2docker/models/runbot_branch.py
@@ -4,8 +4,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import re
-import requests
 from datetime import datetime
+
+import requests
 
 from openerp import fields, models, api
 
