9f64ed40e6b57e00304accd324100069723e00da
Mauricio <mauricio@vauxoo.com>
2018-04-02 11:24:37 -0400

    [IMP] runbot_docker_orchest: add module for create instances with images orchestv

---
+++ runbot_docker_orchest/__manifest__.py
@@ -0,0 +1,25 @@
+# © 2015 Vauxoo
+#   Coded by: moylop260@vauxoo.com
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Runbot orchest to docker",
+    "summary": "Generate docker with odoo instance based on orchest images",
+    "version": "11.0.1.0.0",
+    "category": "runbot",
+    "website": "https://odoo-community.org/",
+    "author": "Vauxoo,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        'runbot'
+    ],
+    "data": [
+        'views/runbot_repo_view.xml',
+        'templates/frontend.xml',
+    ],
+    "demo": [
+        'demo/runbot_repo_demo.xml',
+    ],
+    "application": False,
+    "installable": True,
+}
